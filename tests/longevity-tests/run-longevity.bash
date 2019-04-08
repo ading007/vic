0392d35e676f46285150a2ffc5066e934a18b11f
ading007 <wendingh@vmware.com>
2019-04-09 01:53:27 +0800

    Enable logevity and stress tests

--- tests/longevity-tests/run-longevity.bash
+++ tests/longevity-tests/run-longevity.bash
@@ -90,10 +90,13 @@ input=$(gsutil ls -l gs://vic-engine-builds/vic_* | grep -v TOTAL | sort -k2 -r 
 echo "Downloading VIC build $input..."
 wget https://storage.googleapis.com/vic-engine-builds/${input} -qO - | tar xz -C vic && mv vic/vic vic/bin
 
-docker run -w /go/vic -i \
-    --env-file vic-internal/longevity-${target}-secrets.list \
-    -e SYSLOG_VCH_OPTION="${syslogVchOption}" \
-    -e DEBUG_VCH_LEVEL="${debugVchLevel}" \
-    -v $(pwd)/vic:/go/vic gcr.io/eminent-nation-87317/vic-integration-test:1.48 \
-    pybot tests/manual-test-cases/Group14-Longevity/14-1-Longevity.robot
+#docker run -w /go/vic -i --rm \
+#    --env-file vic-internal/longevity-${target}-secrets.list \
+#    -e SYSLOG_VCH_OPTION="${syslogVchOption}" \
+#    -e DEBUG_VCH_LEVEL="${debugVchLevel}" \
+#    -v $(pwd)/vic:/go/vic gcr.io/eminent-nation-87317/vic-integration-test:1.48 \
+#    pybot tests/manual-test-cases/Group14-Longevity tests/manual-test-cases/Group11-Stress
+
+# clean up
+#rm -rf vic/bin
 
